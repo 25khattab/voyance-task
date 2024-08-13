ee925ef7c1952ef001d0dbfd4b295b75051c6c3d
okhattab <25khattab@gmail.com>
2024-08-14 02:29:20 +0300

    add view toggler for admin

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,21 +1,40 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import ViewToggle from "./view-toggle";
 
 export default async function Navbar() {
   const session = await auth();
   return (
     <nav className="flex w-full justify-between border-b border-b-border bg-background px-5 py-3">
       <div className="">
-        <img src={"logo.png"} alt="" className="h-10 w-10" />
+        <Link href={"/"}>
+          <img src={"logo.png"} alt="" className="h-10 w-10" />
+        </Link>
       </div>
 
-      <ul>
-        {session === null && (
+      <ul className="flex items-center justify-center gap-3">
+        {session && (
+          <li className="">
+            <ViewToggle />
+          </li>
+        )}
+        {session === null ? (
           <li>
-            <Button asChild variant={"ghost"}>
-              <Link href="/login">Login</Link>
+            <Button  variant={"ghost"}>
+              <a href="/login">Login</a>
             </Button>
+          </li>
+        ) : (
+          <li>
+            <form
+              action={async () => {
+                "use server";
+                await signOut({ redirectTo: "/" });
+              }}
+            >
+              <Button variant={"ghost"}>Logout</Button>
+            </form>
           </li>
         )}
       </ul>
--- src/components/view-toggle.tsx
+++ src/components/view-toggle.tsx
@@ -1,1 +1,25 @@
-export default function ViewToggle(){}
+"use client";
+import { usePathname, useRouter } from "next/navigation";
+import { Switch } from "./ui/switch";
+import { Shield, User } from "lucide-react";
+
+export default function ViewToggle() {
+  const currentPath = usePathname();
+  const navigate = useRouter();
+  return (
+    <div className="flex items-center gap-1 justify-center ">
+      <User/>
+      <Switch className=""
+        onCheckedChange={(value) => {
+          if (value) {
+            navigate.push("/manage");
+          } else {
+            navigate.push("/");
+          }
+        }}
+        checked={currentPath === "/manage"}
+      />
+      <Shield />
+    </div>
+  );
+}
