5c16e0a3f24c482688e2d5299c8ba712a28f47e0
okhattab <25khattab@gmail.com>
2024-08-15 11:40:56 +0300

    protect manage router

--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -1,7 +1,11 @@
+import { auth } from "@/auth";
 import ManageBooking from "@/components/manage-booking";
 import { getAppointments } from "@/data-access/appointments";
+import { redirect } from "next/navigation";
 
 export default async function Manage() {
+  const session = await auth();
+  if (!session || !session.user.admin) redirect("login");
   const appointments = await getAppointments();
   return (
     <main className="flex min-h-screen w-full flex-col items-center justify-between p-24">
