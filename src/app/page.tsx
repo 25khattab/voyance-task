c5ebf68214749111404a249ed9b5ea3c96c7f1e8
okhattab <25khattab@gmail.com>
2024-08-15 11:52:17 +0300

    move loading events to server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
+import { auth } from "@/auth";
 import UserBooking from "@/components/user-booking";
 import { getAppointments } from "@/data-access/appointments";
 
 export default async function Home() {
+  const session = await auth();
   const appointments = await getAppointments();
+  const events = appointments.map((e) => ({
+    date: e.createdAt!,
+    forUser: session === null ? false : session.user.id === e.userId,
+    approved: e.approved ?? false,
+  }));
   return (
     <main className="flex min-h-screen w-full flex-col items-center justify-between p-24">
       <h1>Make An Appointment</h1>
       <div className="">
-        <UserBooking appointments={appointments} />
+        <UserBooking events={events} />
       </div>
     </main>
   );
--- src/components/user-booking/index.tsx
+++ src/components/user-booking/index.tsx
@@ -1,32 +1,30 @@
 "use client";
 import Calendar from "@/components/calendar";
 import BookAppointmentDialog from "@/components/user-booking/book-appointment-dialog";
-import { appointment } from "@/db/schema";
 import { DateClickArg } from "@fullcalendar/interaction/index.js";
-import { useSession } from "next-auth/react";
+
 import { useState } from "react";
 
-export default function UserBooking({ appointments }: { appointments: appointment[] }) {
-  const session = useSession();
+export default function UserBooking({
+  events,
+}: {
+  events: {
+    date: string;
+    forUser: boolean;
+    approved: boolean;
+  }[];
+}) {
   const [dialogState, setDialogState] = useState(false);
   const [bookingDate, setBookingDate] = useState<string | null>(null);
   const handleDateClick = (arg: DateClickArg) => {
     if (arg.view.type === "dayGridMonth") return;
-    if (appointments.some((e) => e.createdAt === arg.dateStr)) return;
+    if (events.some((e) => e.date === arg.dateStr)) return;
     setBookingDate(arg.dateStr);
     setDialogState(true);
   };
   return (
     <div className="">
-      <Calendar
-        handleDateClick={handleDateClick}
-        events={appointments.map((e) => ({
-          date: e.createdAt!,
-          forUser: session.status === "unauthenticated" ? false : session.data?.user.id === e.userId,
-          approved: e.approved ?? false,
-        }))}
-        eventContent={renderEventContent}
-      />
+      <Calendar handleDateClick={handleDateClick} events={events} eventContent={renderEventContent} />
       <BookAppointmentDialog open={dialogState} setOpen={setDialogState} bookingDate={bookingDate} />
     </div>
   );
